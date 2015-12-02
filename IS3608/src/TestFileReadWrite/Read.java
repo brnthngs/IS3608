d78f7745e77467856b9a2d9ddfd6bbfb6c542f99
bpwc0b <bpwc0b@ES00504.stl.umsl.edu>
2015-12-01 18:50:32 -0600

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ IS3608/src/TestFileReadWrite/Read.java
@@ -0,0 +1,25 @@
+package TestFileReadWrite;
+
+
+// read.java
+import java.io.*;
+public class Read
+{
+	public static void main(String args[])
+        {
+		Test testobj = null;
+		try
+		{
+			FileInputStream fis = new FileInputStream("file.out");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			testobj = (Test)ois.readObject();
+			fis.close();
+		}
+		catch (Throwable e)
+		{
+			System.err.println(e);
+		}
+		System.out.println(testobj.str);
+		System.out.println(testobj.ivalue);
+	}
+}
---
+++ IS3608/src/TestFileReadWrite/Write.java
@@ -0,0 +1,25 @@
+package TestFileReadWrite;
+
+import java.io.*;
+public class Write 
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            FileOutputStream fos = new FileOutputStream("./cheekymonkey/file.out");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            //Test x = new Test("testing", 37);
+            Test y = new Test();
+            y.str = "testing";
+            y.ivalue = 37;
+            oos.writeObject(y);
+            oos.flush();
+            fos.close();
+        }
+        catch (Throwable e)
+        {
+            System.err.println(e);
+        }
+    }
+}
