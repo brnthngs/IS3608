e792c9ab74429cd3dd6acec2001da07144133763
Benjamin <Benjamin@Jezabelle>
2015-12-05 21:56:44 -0600

    Updated ATM and Account, moved account based menu to Account,

---
+++ IS3608/src/HomeAndLearnUK/textfiles.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HomeAndLearnUK;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+
+        
+
+/**
+ *
+ * @author Benjamin
+ */
+public class textfiles 
+{
+    public static void main(String[] args)throws IOException
+    {
+        
+    }
+    
+    
+}
